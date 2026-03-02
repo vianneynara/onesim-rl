555f975f0a5563ce9e457dd0c4c51d89dc9e9094
vianneynara <narwandaru.nara@gmail.com>
2026-03-02 16:52:21 +0700

    feat: trajectory length reporting interface
    Took 37 minutes

---
+++ src/report/TrajectoryLengthReporter.java
@@ -0,0 +1,16 @@
+package report;
+
+import java.util.Map;
+
+/**
+ * Helps to report trajectory lengths for analyzing the distribution of step length
+ * that Lévy Flight generates and compare it with other movement's generated paths.
+ * */
+public interface TrajectoryLengthReporter {
+	/**
+	 * Retrieves the frequencies of each recorded trajectory length and its frequencies.
+	 * {@link Double} being the length of the trajectory and {@link Integer} being the frequency of that length.
+	 * @return map of trajectory length and frequencies.
+	 * */
+	Map<Double, Integer> getTrajectoryFrequencies();
+}
