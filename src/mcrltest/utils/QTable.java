8ebae42ca5b77b05243f575a396e1e3607d645d7
ZeroKampus <jordanjv453@gmail.com>
2026-03-02 17:01:15 +0700

    compile update

---
+++ src/mcrltest/utils/QTable.java
@@ -0,0 +1,16 @@
+package mcrltest.utils;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class QTable {
+    private Map<Integer, double[]> table;
+
+    public QTable(){
+        table = new HashMap<>();
+    }
+
+    private void initState(int state) {
+        table.putIfAbsent
+    }
+}
