67742a432e91baf3648b8c008700748d90a7ea8d
ZeroKampus <jordanjv453@gmail.com>
2026-03-03 10:16:00 +0700

    feat: add Q-table state container storing Q(s,0) and Q(s,1)

--- src/mcrltest/utils/QTable.java
+++ src/mcrltest/utils/QTable.java
@@ -2,15 +2,39 @@ package mcrltest.utils;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Random;
 
 public class QTable {
-    private Map<Integer, double[]> table;
+    private Map<Integer, QValue> table;
+    private Random random;
 
-    public QTable(){
+    public QTable() {
         table = new HashMap<>();
+        random = new Random();
     }
 
-    private void initState(int state) {
-        table.putIfAbsent
+    private void initializeState(int state) {
+        table.putIfAbsent(state, new QValue());
+    }
+
+    public double getQValue(int state, int action) {
+        initializeState(state);
+        return table.get(state).get(action);
+    }
+
+    public void setQValue(int state, int action, double value) {
+        initializeState(state);
+        table.get(state).set(action, value);
+    }
+
+    public int getBestAction(int state) {
+        initializeState(state);
+        return table.get(state).getBestAction();
+    }
+
+    public void printTable() {
+        for (Map.Entry<Integer, QValue> entry : table.entrySet()) {
+            System.out.println("State " + entry.getKey() + " -> " + entry.getValue());
+        }
     }
 }
