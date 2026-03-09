d8714bc74d2a60c9f6cf459527ab4409ef35f40b
ZeroKampus <jordanjv453@gmail.com>
2026-03-09 14:54:28 +0700

    added description

--- src/mcrltest/qModel/QLearningModel.java
+++ src/mcrltest/qModel/QLearningModel.java
@@ -8,16 +8,24 @@ public class QLearningModel extends RLModel {
         super(s);
     }
 
+    /**
+     * Q(s,a) ← Q(s,a) + α[r + γ * maxQ(s′,a′) − Q(s,a)]
+     *
+     * @param state
+     * @param action
+     * @param reward
+     * @param nextState
+     */
     @Override
     public void update(int state, int action, double reward, int nextState) {
 
-        double currentQ = qTable.getQValue(state, action);
+        double currentQ = qTable.getQValue(state, action);      // Q(s,a)
 
-        double nextMaxQ = qTable.getMaxValue(nextState);
+        double nextMaxQ = qTable.getMaxValue(nextState);        // maxQ(s′,a′)
 
-        double target = reward + gamma * nextMaxQ;
+        double target = reward + gamma * nextMaxQ;              // r + γ * maxQ(s′,a′)
 
-        double newQ = currentQ + alpha * (target - currentQ);
+        double newQ = currentQ + alpha * (target - currentQ);   // Q(s,a) ← Q(s,a) + α[r + γ * maxQ(s′,a′) − Q(s,a)]
 
         qTable.setQValue(state, action, newQ);
     }
