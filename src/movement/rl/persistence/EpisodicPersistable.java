420b5509e0878d056692b8e2a5503bde70b2e420
vianneynara <narwandaru.nara@gmail.com>
2026-03-18 22:13:55 +0700

    feat(EpisodicPersistable): added save/load default wrapper method
    Took 1 hour 16 minutes

--- src/movement/rl/persistence/EpisodicPersistable.java
+++ src/movement/rl/persistence/EpisodicPersistable.java
@@ -1,5 +1,7 @@
 package movement.rl.persistence;
 
+import report.Report;
+
 /**
  * Implemented by classes requiring episodic data persistence.
  *
@@ -9,6 +11,7 @@ public interface EpisodicPersistable {
 
 	/**
 	 * Saves by passing an empty persistence class.
+	 * Used to make sure the implementor separates the logic of saving the persistable dataa.
 	 *
 	 * @param epd
 	 * */
@@ -16,8 +19,30 @@ public interface EpisodicPersistable {
 
 	/**
 	 * Loads the implementor's components from the passed data.
+	 * Used to make sure the implementor separates the logic of loading the persistable dataa.
 	 *
 	 * @param epd
 	 * */
 	void loadFrom(EpisodicPersistenceData epd);
+
+	// These methods are called from the reporter.
+	// Only works if the implementors overrides them with saving/loading inner logic.
+
+	/**
+	 * Called by the reporters to load.
+	 * I think it would work though, since the reporter are only added and instantiated after the hosts are
+	 * created in {@link core.SimScenario} during the scenario's hosts creation (which instantiates the
+	 * movements, routers, etc.).
+	 * Not currently implemented.
+	 * */
+	default void loadPreviousEpisode() {
+		System.out.println("[EpisodicPersistable] Loading previous episode data not implemented");
+	}
+
+	/**
+	 * Used by {@link report.EpisodicManagerReport} to save the current episode data during {@link Report#done()}.
+	 * */
+	default void saveCurrentEpisode() {
+		System.out.println("[EpisodicPersistable] Saving current episode data not implemented");
+	}
 }
