4f847cc26ce90468d273417b4f218a1c4eb87486
ZeroKampus <jordanjv453@gmail.com>
2026-03-23 11:34:25 +0700

    feat: add total reward

--- src/mcrltest/qModel/QLearningModel.java
+++ src/mcrltest/qModel/QLearningModel.java
@@ -10,25 +10,20 @@ public class QLearningModel extends RLModel {
 
     /**
      * Q(s,a) ← Q(s,a) + α[r + γ * maxQ(s′,a′) − Q(s,a)]
-     *
-     * @param state
-     * @param action
-     * @param reward
-     * @param nextState
      */
     @Override
     public void update(int state, int action, double reward, int nextState) {
 
-        double currentQ = qTable.getQValue(state, action);      // Q(s,a)
+        double currentQ = qTable.getQValue(state, action);   // Q(s,a)
 
-        double nextMaxQ = qTable.getMaxValue(nextState);        // maxQ(s′,a′)
+        double nextMaxQ = qTable.getMaxValue(nextState);     // maxQ(s',a')
 
-        double target = reward + gamma * nextMaxQ;              // r + γ * maxQ(s′,a′)
+        double target = reward + gamma * nextMaxQ;           // r + γ maxQ
 
-        double newQ = currentQ + alpha * (target - currentQ);   // Q(s,a) ← Q(s,a) + α[r + γ * maxQ(s′,a′) − Q(s,a)]
-
-        totalTrainingReward += reward;
+        double newQ = currentQ + alpha * (target - currentQ);
 
         qTable.setQValue(state, action, newQ);
+
+        totalTrainingReward += reward;
     }
 }
