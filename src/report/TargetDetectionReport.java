25acac9ea505a953c335c9fcac131418d858cc73
ZeroKampus <jordanjv453@gmail.com>
2026-03-27 16:06:55 +0700

    feat: build target detection report

---
+++ src/report/TargetDetectionReport.java
@@ -0,0 +1,56 @@
+package report;
+
+import core.DTNHost;
+import core.SimScenario;
+import mcrltest.agent.RLAgent;
+import mcrltest.utils.DetectionInfo;
+import mcrltest.utils.EpisodeStep;
+import movement.RLMovementModel;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class TargetDetectionReport extends Report {
+
+    private Map<DTNHost, Integer> targetDetection;
+
+    public TargetDetectionReport() {
+        super();
+        this.targetDetection = new HashMap<>();
+    }
+
+    @Override
+    public void done() {
+            /* ===== GET HOSTS PROPERLY ===== */
+            List<DTNHost> hosts = SimScenario.getInstance().getHosts();
+
+            for (DTNHost host : hosts) {
+
+                if (host.getMovement() instanceof RLMovementModel) {
+
+                    RLMovementModel movement =
+                            (RLMovementModel) host.getMovement();
+
+                    Map<DTNHost, DetectionInfo> objectiveFound = movement.getObjectiveFound();
+
+                    for (Map.Entry<DTNHost, DetectionInfo> step : objectiveFound.entrySet()) {
+//                        String name = step.getKey().getName();
+                        DTNHost theHost = step.getKey();
+                        int count = step.getValue().getNrofMeet();
+
+                        targetDetection.put(theHost, count);
+                    }
+                }
+            }
+
+            write("Total Target Found: " + targetDetection.size() + "/" + hosts.size());
+
+            /* ===== WRITE RESULT ===== */
+            for (Map.Entry<DTNHost, Integer> entry : targetDetection.entrySet()) {
+                write(entry.getKey().getName() + "," + entry.getValue());
+            }
+
+        super.done();
+    }
+}
