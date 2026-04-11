15239d438e7ffb0412bae732a818250342d8199d
vianneynara <narwandaru.nara@gmail.com>
2026-04-11 17:31:36 +0700

    refactor(conf): more compact format
    Took 2 minutes

--- batch_configs.py
+++ batch_configs.py
@@ -4,11 +4,7 @@ The list of configurations for the batch runner. Starts from index 0.
 
 LIST_OF_CONFIGS = [
     # [ Q-Learning with Epsilon-Greedy
-    {
-        "alg": "ql",
-        "runs": 5,
-        "bp": "epsilon",
-        "id": "ql5-bp=epsilon",
+    {"alg": "ql", "runs": 5, "bp": "epsilon", "id": "ql5-bp=epsilon",
         "overrides": {
             "eg_ip": 1.0,
             "eg_ed": 0.999999,
@@ -17,22 +13,14 @@ LIST_OF_CONFIGS = [
     },
 
     # [ Q-Learning with UCB ]
-    {
-        "alg": "ql",
-        "runs": 5,
-        "bp": "ucb",
-        "id": "ql5-bp=ucb",
+    {"alg": "ql", "runs": 5, "bp": "ucb", "id": "ql5-bp=ucb",
         "overrides": {
             "ucb_ec": 2.0,
         }
     },
 
     # [ Q-Learning with Thompson Sampling ]
-    {
-        "alg": "ql",
-        "runs": 5,
-        "bp": "ts",
-        "id": "ql5-bp=ts",
+    {"alg": "ql", "runs": 5, "bp": "ts", "id": "ql5-bp=ts",
         "overrides": {
             "ts_iv": 1.0,
         }
