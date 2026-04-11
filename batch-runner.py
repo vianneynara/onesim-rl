adff4a4b060cc9d9ac261a9eb59df4ec0ac0114d
vianneynara <narwandaru.nara@gmail.com>
2026-04-11 16:06:40 +0700

    feat(runner): this one works lol
    Took 39 minutes

--- batch-runner.py
+++ batch-runner.py
@@ -117,7 +117,7 @@ LIST_OF_CONFIGS = [
         "alg": "ql",
         "runs": 5,
         "bp": "epsilon",
-        "id": "ql5&bp=epsilon",
+        "id": "ql5-bp=epsilon",
         "overrides": {
             "eg_ip": 1.0,
             "eg_ed": 0.999999,
@@ -128,7 +128,7 @@ LIST_OF_CONFIGS = [
         "alg": "ql",
         "runs": 5,
         "bp": "ucb",
-        "id": "ql5&bp=ucb",
+        "id": "ql5-bp=ucb",
         "overrides": {
             "ucb_ec": 2.0,
         }
@@ -137,7 +137,7 @@ LIST_OF_CONFIGS = [
         "alg": "ql",
         "runs": 5,
         "bp": "ts",
-        "id": "ql5&bp=ts",
+        "id": "ql5-bp=ts",
         "overrides": {
             "ts_iv": 1.0,
         }
@@ -158,7 +158,7 @@ def parse_overrides(overrides_dict: dict[str]) -> str:
             # Use the key as-is (assuming the real setting full key)
             full_key = key
 
-        abr_overrides_list.append(f"{key}={value}")
+        abr_overrides_list.append(f"{key}@{value}")
         full_overrides_list.append(f"{full_key}={value}")
 
     return abr_overrides_list, full_overrides_list
@@ -174,10 +174,10 @@ def expand_algorithm(alg: str) -> str:
 
 def build_result_id_dir(alg: str, runs: int, bp: str = None, overrides: list[str] = None) -> str:
     # Initial first part  of algorithm and runs
-    result_id_dir = f"{alg}{runs}&qlm_bp={bp}&"
+    result_id_dir = f"{alg}{runs}-qlm_bp@{bp}-"
 
     # Adds overrides if exist
-    result_id_dir += "&".join(overrides) if overrides else ""
+    result_id_dir += "-".join(overrides) if overrides else ""
 
     return result_id_dir
 
@@ -218,20 +218,22 @@ def run_simulation(alg: str, runs: int, bp: str, run_id: str = None, overrides_l
             f"Unknown behavior policy '{bp}'. Valid options: {', '.join(behavior_packages.keys())}"
         )
 
-    result_id_dir = build_result_id_dir(alg, runs, bp, overrides_list)
+    abr_overrides, full_overrides = [], []
+
+    # Build the base overrides
+    if overrides_list:
+        abr_overrides, full_overrides  = parse_overrides(overrides_list)
+
+    result_id_dir = build_result_id_dir(alg, runs, bp, abr_overrides)
     validate_run_id(result_id_dir)
     full_report_dir = f"reports/skripsi/{alg}/run-id/{result_id_dir}"
 
-    # Build the base overrides
     bp_override = f"QLearningMovement.behaviorPolicy={behavior_packages[bp]}"
     persistence_override = f"EpisodicPersistenceManager.persistencePath={full_report_dir}/_persistence.json"
 
-    abr_overrides, full_overrides = [], []
-    if overrides_list:
-        abr_overrides, full_overrides  = parse_overrides(overrides_list)
-        if full_overrides:
-            full_overrides.append(bp_override)
-            full_overrides.append(persistence_override)
+    if overrides_list and full_overrides:
+        full_overrides.append(bp_override)
+        full_overrides.append(persistence_override)
 
     overrides_string = "@@".join(full_overrides)
 
@@ -244,9 +246,9 @@ def run_simulation(alg: str, runs: int, bp: str, run_id: str = None, overrides_l
     print(f"{'=' * 70}\n")
 
     # Execute episodes
-    successes = 0
-    failures = 0
-    start_time = datetime.now().second
+    succeeds = 0
+    failed = 0
+    start_time = datetime.now()
 
     for ep in range(1, runs + 1):
         running_overrides_string = (
@@ -255,21 +257,27 @@ def run_simulation(alg: str, runs: int, bp: str, run_id: str = None, overrides_l
                 + f"@@EpisodicPersistenceManager.episodeNumber={str(ep)}"
         )
         if run_script(alg, running_overrides_string, ep):
-            successes += 1
+            succeeds += 1
         else:
-            failures += 1
+            failed += 1
 
-    end_time = datetime.now().second
+    end_time = datetime.now()
 
     # Print summary
     print(f"\n{'=' * 70}")
-    print(f"[INFO] Episodic simulation batch completed at {str(dt.timedelta(end_time))}, time taken: {str(dt.timedelta(end_time - start_time))}")
-    print(f"[INFO] Total episodes: {runs}")
-    print(f"[INFO] Successful: {successes}")
-    print(f"[INFO] Failed: {failures}")
+    print(f"[INFO] Episodic simulation batch completed at {end_time}, time taken: {format_timedelta(end_time-start_time)}")
+    print(f"[INFO] Total episodes: {runs} (Success: {succeeds}, Fails: {failed})")
     print(f"{'=' * 70}\n")
 
-    return failures == 0
+    return failed == 0
+
+
+def format_timedelta(td):
+    # Calculate total components
+    total_seconds = int(td.total_seconds())
+    hours, remainder = divmod(total_seconds, 3600)
+    minutes, seconds = divmod(remainder, 60)
+    return f"{hours:02}:{minutes:02}:{seconds:02}"
 
 
 if __name__ == "__main__":
@@ -277,33 +285,56 @@ if __name__ == "__main__":
         description="ONE Simulator multi-run/episodic launcher (version 2), simpler"
     )
     parser.add_argument(
-        "-c", "--config", type=int, help="Config index to run", required=True
+        "-c", "--config", type=int, help="Config index to run", required=False
+    )
+
+    parser.add_argument(
+        "-a", "--all", action="store_true", help="Run all configs"
     )
 
     # Parse arguments
     args = parser.parse_args()
 
-    # Dispatch to appropriate handler
-    # try:
-    config_idx = args.config
-    if config_idx < 0 or config_idx >= len(LIST_OF_CONFIGS):
-        raise ValueError(
-            f"Config index {config_idx} out of range [0, {len(LIST_OF_CONFIGS) - 1}]"
-        )
-    config = LIST_OF_CONFIGS[config_idx]
-    alg = config["alg"]
-    runs = config["runs"]
-    bp = config["bp"]
-    id = config["id"]
-    overrides = config["overrides"] if "overrides" in config else args.d
+    success = False
+    if args.all:
+        successes = 0
+        failures = 0
 
-    # Execute simulation
-    success = run_simulation(
-        alg=alg,
-        runs=runs,
-        bp=bp,
-        run_id=id,
-        overrides_list=overrides
-    )
+        for config in LIST_OF_CONFIGS:
+            alg = config["alg"]
+            runs = config["runs"]
+            bp = config["bp"]
+            id = config["id"]
+            overrides = config["overrides"] if "overrides" in config else args.d
+
+            # Execute simulation
+            success = run_simulation(
+                alg=alg,
+                runs=runs,
+                bp=bp,
+                run_id=id,
+                overrides_list=overrides
+            )
+    else:
+        config_idx = args.config
+        if config_idx < 0 or config_idx >= len(LIST_OF_CONFIGS):
+            raise ValueError(
+                f"Config index {config_idx} out of range [0, {len(LIST_OF_CONFIGS) - 1}]"
+            )
+        config = LIST_OF_CONFIGS[config_idx]
+        alg = config["alg"]
+        runs = config["runs"]
+        bp = config["bp"]
+        id = config["id"]
+        overrides = config["overrides"] if "overrides" in config else args.d
+
+        # Execute simulation
+        success = run_simulation(
+            alg=alg,
+            runs=runs,
+            bp=bp,
+            run_id=id,
+            overrides_list=overrides
+        )
 
     sys.exit(0 if success else 1)
