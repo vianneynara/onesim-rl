14e0460b917edcafdc0fad27add6ce21b5504811
vianneynara <narwandaru.nara@gmail.com>
2026-05-23 12:44:57 +0700

    conf(script): use scripts for reverting n unfinished running

--- cmd_scripts_runner.py
+++ cmd_scripts_runner.py
@@ -29,11 +29,25 @@ VENV_ACTIVATE = WORK_DIR / ".venv" / "Scripts" / "activate.bat"
 #     r'python pyplotters/persistence_plotter.py -pid ql-p-ms@1 --title "Q-Learning on Fixed Stationary Poisson Distributed Targets" --describe',
 # ]
 
+# commands = [
+#     r'python pyrunner/batch_runner.py -pid ql-c-ms@1 -c 11-13 -alg ql-c-ms@1 -vc',
+#     r'python pyrunner/batch_runner.py -pid ql-c-ms@1 -c 14-15 -alg ql-c-ms@1 -vc',
+#     r'python pyrunner/batch_runner.py -pid ql-c-ms@1 -c 16-19 -alg ql-c-ms@1 -vc',
+#     r'python pyrunner/batch_runner.py -pid ql-c-ms@1 -c 20-22 -alg ql-c-ms@1 -vc',
+# ]
+
+# commands = [
+#     r'python pyrunner/batch_runner.py -pid ql-c-ms@0 -c 25 -alg ql-c-ms@0 -vc',
+#     r'python pyrunner/batch_runner.py -pid ql-c-ms@0 -c 32 -alg ql-c-ms@0 -vc',
+#     r'python pyrunner/batch_runner.py -pid ql-p-ms@0 -c 27 -alg ql-p-ms@0 -vc',
+#     r'python pyrunner/batch_runner.py -pid ql-p-ms@0 -c 31 -alg ql-p-ms@0 -vc',
+# ]
+
 commands = [
-    r'python pyrunner/batch_runner.py -pid ql-c-ms@1 -c 11-13 -alg ql-c-ms@1 -vc',
-    r'python pyrunner/batch_runner.py -pid ql-c-ms@1 -c 14-15 -alg ql-c-ms@1 -vc',
-    r'python pyrunner/batch_runner.py -pid ql-c-ms@1 -c 16-19 -alg ql-c-ms@1 -vc',
-    r'python pyrunner/batch_runner.py -pid ql-c-ms@1 -c 20-22 -alg ql-c-ms@1 -vc',
+    r'python pyrunner/episode_extender.py -pid ql-c-ms@0 -c 25 --revertto 100',
+    r'python pyrunner/episode_extender.py -pid ql-c-ms@0 -c 32 --revertto 100',
+    r'python pyrunner/episode_extender.py -pid ql-p-ms@0 -c 27 --revertto 100',
+    r'python pyrunner/episode_extender.py -pid ql-p-ms@0 -c 31 --revertto 100',
 ]
 
 # Open each command in a new Command Prompt window
