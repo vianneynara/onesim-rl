96da39607c4edc5d75649524a58a8435128ee770
vianneynara <narwandaru.nara@gmail.com>
2026-05-23 12:24:13 +0700

    conf(script): use scripts for unfinished running

--- cmd_scripts_runner.py
+++ cmd_scripts_runner.py
@@ -1,8 +1,10 @@
 import subprocess
 import os
+from pathlib import Path
 
-# Working directory
-WORK_DIR = r"C:\Users\jarkom\Documents\Java\onesim-rl"
+# Working directory - dynamically detect repo root where this script is located
+WORK_DIR = Path(__file__).resolve().parent
+VENV_ACTIVATE = WORK_DIR / ".venv" / "Scripts" / "activate.bat"
 
 # # Commands to run (batch urnning)
 # commands = [
@@ -13,43 +15,33 @@ WORK_DIR = r"C:\Users\jarkom\Documents\Java\onesim-rl"
 #     r'python pyrunner/batch_runner.py -pid ql-c-ms@0 -alg ql-c-ms@0 -c 17-22 -vc',
 #     r'python pyrunner/batch_runner.py -pid ql-c-ms@1 -alg ql-c-ms@1 -c 17-22 -vc',
 #     r'python pyrunner/batch_runner.py -pid ql-p-ms@0 -alg ql-p-ms@0 -c 17-22 -vc',
-#     r'python pyrunner/batch_runner.py -pid ql-p-ms@1 -alg ql-p-ms@1 -c 17-22 -vc',
 # ]
-
-# Commands to run (batch urnning)
-commands = [
-    r'python pyrunner/batch_runner.py -pid ql-p-ms@0 -alg ql-p-ms@0 -c 11 -vc',
-    r'python pyrunner/batch_runner.py -pid ql-p-ms@0 -alg ql-p-ms@0 -c 12 -vc',
-    r'python pyrunner/batch_runner.py -pid ql-p-ms@0 -alg ql-p-ms@0 -c 13 -vc',
-    r'python pyrunner/batch_runner.py -pid ql-p-ms@0 -alg ql-p-ms@0 -c 14 -vc',
-    r'python pyrunner/batch_runner.py -pid ql-p-ms@0 -alg ql-p-ms@0 -c 15 -vc',
-    r'python pyrunner/batch_runner.py -pid ql-p-ms@0 -alg ql-p-ms@0 -c 16 -vc',
-    r'python pyrunner/batch_runner.py -pid ql-p-ms@0 -alg ql-p-ms@0 -c 17 -vc',
-    r'python pyrunner/batch_runner.py -pid ql-p-ms@0 -alg ql-p-ms@0 -c 18 -vc',
-    r'python pyrunner/batch_runner.py -pid ql-p-ms@0 -alg ql-p-ms@0 -c 19 -vc',
-    r'python pyrunner/batch_runner.py -pid ql-p-ms@0 -alg ql-p-ms@0 -c 20 -vc',
-    r'python pyrunner/batch_runner.py -pid ql-p-ms@0 -alg ql-p-ms@0 -c 21 -vc',
-    r'python pyrunner/batch_runner.py -pid ql-p-ms@0 -alg ql-p-ms@0 -c 22 -vc',
-]
 
 # # Commands to run, all base plotting
 # commands = [
-#     r'python pyplotters/persistence_plotter.py -pid lfe-c-ms@0 --title "Lévy Flight on Stochastic Stationary Clustered Distributed Targets" --describe',
+#     r'python pyplotters/persistence_plotter.py -pid lfe-c-ms@0 --title "Lévy Flight on Resampled Stationary Clustered Distributed Targets" --describe',
 #     r'python pyplotters/persistence_plotter.py -pid lfe-c-ms@1 --title "Lévy Flight on Fixed Stationary Clustered Distributed Targets" --describe',
-#     r'python pyplotters/persistence_plotter.py -pid lfe-p-ms@0 --title "Lévy Flight on Stochastic Stationary Poisson Distributed Targets" --describe',
+#     r'python pyplotters/persistence_plotter.py -pid lfe-p-ms@0 --title "Lévy Flight on Resampled Stationary Poisson Distributed Targets" --describe',
 #     r'python pyplotters/persistence_plotter.py -pid lfe-p-ms@1 --title "Lévy Flight on Fixed Stationary Poisson Distributed Targets" --describe',
-#     r'python pyplotters/persistence_plotter.py -pid ql-c-ms@0 --title "Q-Learning on Stochastic Stationary Clustered Distributed Targets" --describe',
+#     r'python pyplotters/persistence_plotter.py -pid ql-c-ms@0 --title "Q-Learning on Resampled Stationary Clustered Distributed Targets" --describe',
 #     r'python pyplotters/persistence_plotter.py -pid ql-c-ms@1 --title "Q-Learning on Fixed Stationary Clustered Distributed Targets" --describe',
-#     r'python pyplotters/persistence_plotter.py -pid ql-p-ms@0 --title "Q-Learning on Stochastic Stationary Poisson Distributed Targets" --describe',
+#     r'python pyplotters/persistence_plotter.py -pid ql-p-ms@0 --title "Q-Learning on Resampled Stationary Poisson Distributed Targets" --describe',
 #     r'python pyplotters/persistence_plotter.py -pid ql-p-ms@1 --title "Q-Learning on Fixed Stationary Poisson Distributed Targets" --describe',
 # ]
 
+commands = [
+    r'python pyrunner/batch_runner.py -pid ql-c-ms@1 -c 11-13 -alg ql-c-ms@1 -vc',
+    r'python pyrunner/batch_runner.py -pid ql-c-ms@1 -c 14-15 -alg ql-c-ms@1 -vc',
+    r'python pyrunner/batch_runner.py -pid ql-c-ms@1 -c 16-19 -alg ql-c-ms@1 -vc',
+    r'python pyrunner/batch_runner.py -pid ql-c-ms@1 -c 20-22 -alg ql-c-ms@1 -vc',
+]
+
 # Open each command in a new Command Prompt window
-for cmd in commands:
+for command in commands:
     full_cmd = (
-        f'start "" cmd /k "'
-        f'cd /d "{WORK_DIR}" '
-        f'&& {cmd}"'
+        f'start "" cmd /k "cd /d \"{WORK_DIR}\" && '
+        f'call \"{VENV_ACTIVATE}\" && '
+        f'{command}"'
     )
 
     subprocess.Popen(full_cmd, shell=True)
