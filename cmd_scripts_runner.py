a0fdfb0016ca613f70d1c55b4c733805c4254ab9
vianneynara <narwandaru.nara@gmail.com>
2026-05-25 16:24:18 +0700

    conf(script): prepare lfe and ql scripts with gaussian environments

--- cmd_scripts_runner.py
+++ cmd_scripts_runner.py
@@ -6,48 +6,18 @@ from pathlib import Path
 WORK_DIR = Path(__file__).resolve().parent
 VENV_ACTIVATE = WORK_DIR / ".venv" / "Scripts" / "activate.bat"
 
-# # Commands to run (batch urnning)
-# commands = [
-#     r'python pyrunner/batch_runner.py -pid ql-c-ms@0 -alg ql-c-ms@0 -c 11-16 -vc',
-#     r'python pyrunner/batch_runner.py -pid ql-c-ms@1 -alg ql-c-ms@1 -c 11-16 -vc',
-#     r'python pyrunner/batch_runner.py -pid ql-p-ms@0 -alg ql-p-ms@0 -c 11-16 -vc',
-#     r'python pyrunner/batch_runner.py -pid ql-p-ms@1 -alg ql-p-ms@1 -c 11-16 -vc',
-#     r'python pyrunner/batch_runner.py -pid ql-c-ms@0 -alg ql-c-ms@0 -c 17-22 -vc',
-#     r'python pyrunner/batch_runner.py -pid ql-c-ms@1 -alg ql-c-ms@1 -c 17-22 -vc',
-#     r'python pyrunner/batch_runner.py -pid ql-p-ms@0 -alg ql-p-ms@0 -c 17-22 -vc',
-# ]
+# -srp "D:\Developments+\Java\onesim-rl-data\reports"
 
-# # Commands to run, all base plotting
-# commands = [
-#     r'python pyplotters/persistence_plotter.py -pid lfe-c-ms@0 --title "Lévy Flight on Resampled Stationary Clustered Distributed Targets" --describe',
-#     r'python pyplotters/persistence_plotter.py -pid lfe-c-ms@1 --title "Lévy Flight on Fixed Stationary Clustered Distributed Targets" --describe',
-#     r'python pyplotters/persistence_plotter.py -pid lfe-p-ms@0 --title "Lévy Flight on Resampled Stationary Poisson Distributed Targets" --describe',
-#     r'python pyplotters/persistence_plotter.py -pid lfe-p-ms@1 --title "Lévy Flight on Fixed Stationary Poisson Distributed Targets" --describe',
-#     r'python pyplotters/persistence_plotter.py -pid ql-c-ms@0 --title "Q-Learning on Resampled Stationary Clustered Distributed Targets" --describe',
-#     r'python pyplotters/persistence_plotter.py -pid ql-c-ms@1 --title "Q-Learning on Fixed Stationary Clustered Distributed Targets" --describe',
-#     r'python pyplotters/persistence_plotter.py -pid ql-p-ms@0 --title "Q-Learning on Resampled Stationary Poisson Distributed Targets" --describe',
-#     r'python pyplotters/persistence_plotter.py -pid ql-p-ms@1 --title "Q-Learning on Fixed Stationary Poisson Distributed Targets" --describe',
-# ]
-
-# commands = [
-#     r'python pyrunner/batch_runner.py -pid ql-c-ms@1 -c 11-13 -alg ql-c-ms@1 -vc',
-#     r'python pyrunner/batch_runner.py -pid ql-c-ms@1 -c 14-15 -alg ql-c-ms@1 -vc',
-#     r'python pyrunner/batch_runner.py -pid ql-c-ms@1 -c 16-19 -alg ql-c-ms@1 -vc',
-#     r'python pyrunner/batch_runner.py -pid ql-c-ms@1 -c 20-22 -alg ql-c-ms@1 -vc',
-# ]
-
-# commands = [
-#     r'python pyrunner/batch_runner.py -pid ql-c-ms@0 -c 25 -alg ql-c-ms@0 -vc',
-#     r'python pyrunner/batch_runner.py -pid ql-c-ms@0 -c 32 -alg ql-c-ms@0 -vc',
-#     r'python pyrunner/batch_runner.py -pid ql-p-ms@0 -c 27 -alg ql-p-ms@0 -vc',
-#     r'python pyrunner/batch_runner.py -pid ql-p-ms@0 -c 31 -alg ql-p-ms@0 -vc',
-# ]
-
+# COMMAND-FOR: Running all of the Immobile Gaussian and Immobile Non-Stationary Gaussian
 commands = [
-    r'python pyrunner/episode_extender.py -pid ql-c-ms@0 -c 25 --revertto 100',
-    r'python pyrunner/episode_extender.py -pid ql-c-ms@0 -c 32 --revertto 100',
-    r'python pyrunner/episode_extender.py -pid ql-p-ms@0 -c 27 --revertto 100',
-    r'python pyrunner/episode_extender.py -pid ql-p-ms@0 -c 31 --revertto 100',
+    r'python pyrunner/batch_runner.py -pid lfe-g-ms@0 -c 36-43 -alg lfe-g-ms@0',
+    r'python pyrunner/batch_runner.py -pid lfe-g-ms@1 -c 36-43 -alg lfe-g-ms@1',
+    r'python pyrunner/batch_runner.py -pid ql-g-ms@0 -c 1-35 -alg ql-g-ms@0',
+    r'python pyrunner/batch_runner.py -pid ql-g-ms@1 -c 1-35 -alg ql-g-ms@1',
+    r'python pyrunner/batch_runner.py -pid lfe-nsg-ms@0 -c 36-43 -alg lfe-nsg-ms@0',
+    r'python pyrunner/batch_runner.py -pid lfe-nsg-ms@1 -c 36-43 -alg lfe-nsg-ms@1',
+    r'python pyrunner/batch_runner.py -pid ql-nsg-ms@0 -c 1-35 -alg ql-nsg-ms@0',
+    r'python pyrunner/batch_runner.py -pid ql-nsg-ms@1 -c 1-35 -alg ql-nsg-ms@1',
 ]
 
 # Open each command in a new Command Prompt window
